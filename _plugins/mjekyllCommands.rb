37696cc04b629cf55b1c6dfcd0867fdb4df62902
Stephen Gaito <stephen@perceptisys.co.uk>
2019-04-03 11:38:18 +0100

    refactored refs plugins to allow them to be loaded from common

---
+++ _plugins/mjekyllCommands.rb
@@ -0,0 +1,10 @@
+# This mjekyll ruby plugin loads the mjekyll plugins required for this 
+# mjekyll sub-website.
+
+# The following order is CRITICAL!
+
+require 'xapianRefsIndexer'
+require 'refsBuilder'
+require 'newRefsCommands'
+require 'propagateRefsCommands'
+require 'refsLiquidTags'
